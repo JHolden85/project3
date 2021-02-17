762fd3b2a7c19cf9229edf0f4e76f26140527154
Carlos <perez.carlos_a@yahoo.com>
2021-02-16 21:40:10 -0800

    commiting to merge!

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,39 +5,15 @@ const controller = require('../../controllers/teamController');
 router.get('/', (req, res) => {
 	//this is the route to get current user info
 
-	// controller.findAll();
+	controller.findAll({});
 
-	db.Team.find({})
-		.then((teamDB) => {
-			console.log(teamDB);
-			res.json(teamDB);
-		})
-		.catch((err) => res.status(400).json(err));
+	// db.Team.find({})
+	// 	.then((teamDB) => {
+	// 		console.log(teamDB);
+	// 		res.json(teamDB);
+	// 	})
+	// 	.catch((err) => res.status(400).json(err));
 });
-
-// 	  NO API/TEAM PAGE IN REACT - THROWS 404 ERROR
-// /////////////////////////////////////////////////
-
-// router.get('/api/team', (req, res) => {
-// 	//this is the route to get team info
-// 	db.Team.find({})
-// 		.then((dbTeam) => res.json(dbTeam))
-// 		.catch((err) => res.status(400).json(err));
-// });
-
-// router.post('/api/team', (req, res) => {
-// 	db.Team.create(req.body)
-// 		.then((teamMember) => res.json(teamMember))
-// 		.catch((err) => res.status(400).json(err));
-// });
-
-// router.post('/api/team', (req, res) => {
-// 	db.Team.updateOne(req.body)
-// 		.then((teamMember) => res.json(teamMember))
-// 		.catch((err) => res.status(400).json(err));
-// });
-
-// /////////////////////////////////////////////////
 
 router.post('/signup', (req, res) => {
 	//sign up the user
