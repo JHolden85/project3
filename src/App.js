b71db2cd644dff57e461d4d56e619551bc70e0b3
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-02-16 19:28:17 -0800

    Pushing to have a group pull 02/16

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Maps from "./pages/Maps";
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -11,22 +11,7 @@ function App() {
         <Route path="/Maps" component ={Maps} />
       </div>
     </Router>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    
   );
 }
 
