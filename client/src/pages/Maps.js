386462295051b315a83376677cc102694114ad74
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-02-20 10:52:07 -0800

    Changing the data structure with hooks instead

--- client/src/pages/Maps.js
+++ client/src/pages/Maps.js
@@ -1,68 +1,84 @@
-import React, { Component, useState } from "react";
+import React, { useState, useEffect } from "react";
 import {getGoogleMap} from "../utils/API";
 import Container from "../components/Container";
-import MapResults from "../components/mapResults";
+// import MapResults from "../components/mapResults";
 
 
-// Make an iFrame and display the maps
-class Maps extends Component {
-    state= {
-        name:[],
-        photos:[],
-        vicinity:[],
-        error:""
-    };
-// Make sure the function is called on page load
-    componentDidMount() {
-        // function to call the axios request and get the data
-        getGoogleMap("33.771713,-118.18131", 1500, "park")
-        .then(res => {
-            console.log("Maps.js ",res);
-            this.setState({
-                name: res.data.results,
-                photos: res.data.results,
-                vicinity: res.data.results
-            });
-        })
+// Make an iFrame and display the maps FOR SETTING A CLASS
+// class Maps extends Component {
+//     state= {
+//         name:[],
+//         photos:[],
+//         vicinity:[],
+//         error:""
+//     };
+// Make sure the function is called on page load FOR SETTING A CLASS
+//     componentDidMount() {
+//         // function to call the axios request and get the data
+//         getGoogleMap("33.771713,-118.18131", 1500, "park")
+//         .then(res => {
+//             console.log("Maps.js ",res);
+//             this.setState({
+//                 name: res.data.results,
+//                 photos: res.data.results,
+//                 vicinity: res.data.results
+//             });
+//         })
         
-        .catch((err) => console.log(err));    
+//         .catch((err) => console.log(err));    
         
-}
+// }
+//==================================================================
+
+// Setting the developer state
+function Maps() {
+const [developerState, setDeveloperState] =useState({
+    name:[],
+    photos:[],
+    vicinity:[]
+})
+
+
+
 
 // ================= Hooks for setting data ==============
-// import React, { useState, useEffect } from "react";
-// // Setting our component's initial state
-//   const [books, setBooks] = useState([])
+
+// Setting our component's initial state
+  const [name, setName] = useState([])
+  const [photos, setPhotos] =useState([])
+  const [vicinity, setVicinity] =useState([])
 //   const [formObject, setFormObject] = useState({})
-//   // Load all books and store them with setBooks
-//   useEffect(() => {
-//     loadBooks()
-//   }, [])
-//   // Loads all books and sets them to books
-//   function loadBooks() {
-//     API.getBooks()
-//       .then(res => 
-//         setBooks(res.data)
-//       )
-//       .catch(err => console.log(err));
-//   }; (edited)
+//   // Load all parks and store them with setParks
+   useEffect(() => {
+    loadParks()
+   }, [])
+    // Loads all books and sets them to books
+  function loadParks() {
+    getGoogleMap("33.771713,-118.18131", 1500, "park")
+    // .then(res => 
+        // =====
+        // FOR SETTING THE DATA INTO A FUNCTION AND LOAD INTO A DATABASE
+        // setGoogleMap(res.data)
+        // ====
+        // )
+        .catch(err => console.log(err));
+    
+   };
 
-
-
-
-
-render(){
+// render(){
     return(
-        <div>
             <Container style={{minHeight: "80%"}}>
                 <h1 className="text-center">Parks In Your Area</h1>
                 <h2 className="text-center">Please Delete Later</h2>
-            <MapResults/>
-           </Container>
-        </div>
+                
+             
+            <div className="card">
+                <div>Park Name: {developerState.name}</div>
+                </div>
+                </Container>
+                
+           
     )
-}
-
 }
 
 export default Maps;
