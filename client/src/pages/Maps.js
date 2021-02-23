960695ca88d829f644b0dbdb9856d1982b532d3f
Carlos <perez.carlos_a@yahoo.com>
2021-02-22 22:21:30 -0800

    Minor changes. Added Ralf's API functions back in to API.js in react-app

--- client/src/pages/Maps.js
+++ client/src/pages/Maps.js
@@ -1,10 +1,10 @@
-import React, { Component, useState, useEffect } from "react";
-import { getGoogleMap } from "../utils/API";
-import API from "../utils/API";
-import { Link } from "react-router-dom";
+import React, { Component, useState, useEffect } from 'react';
+import { getGoogleMap } from '../utils/API';
+import API from '../utils/API';
+import { Link } from 'react-router-dom';
 
-import Container from "../components/Container";
-const googleApiKey = "AIzaSyBh52MlbJJBGNRjJP5tYFwtdEAiYxLqp2s";
+import Container from '../components/Container';
+const googleApiKey = 'AIzaSyBh52MlbJJBGNRjJP5tYFwtdEAiYxLqp2s';
 
 // import MapResults from "../components/mapResults";
 
@@ -86,162 +86,162 @@ const googleApiKey = "AIzaSyBh52MlbJJBGNRjJP5tYFwtdEAiYxLqp2s";
 // Setting the developer state
 
 function Maps({ setPark }) {
-  //   const [developerState, setDeveloperState] = useState({
-  //     name: [],
-  //     photos: [],
-  //     vicinity: [],
-  //     business_status:""
-  //   });
+	//   const [developerState, setDeveloperState] = useState({
+	//     name: [],
+	//     photos: [],
+	//     vicinity: [],
+	//     business_status:""
+	//   });
 
-  //data destructuring
+	//data destructuring
 
-  // ================= Hooks for setting data ==============
+	// ================= Hooks for setting data ==============
 
-  // Setting our component's initial state
-  //   const [name, setName] = useState([]);
-  //   const [photos, setPhotos] = useState([]);
-  //   const [vicinity, setVicinity] = useState([]);
-  //   const [business_status, setBiz] = useState([]);
-  //   const [formObject, setFormObject] = useState({})
-  const [parks, setParks] = useState([]);
-  //   // Load all parks and store them with setParks
-  useEffect(() => {
-    getGoogleMap("38.57933,-121.4909", 1500, "park").then(({ data }) => {
-      console.log(data);
-      setParks(data);
-      // setName()
+	// Setting our component's initial state
+	//   const [name, setName] = useState([]);
+	//   const [photos, setPhotos] = useState([]);
+	//   const [vicinity, setVicinity] = useState([]);
+	//   const [business_status, setBiz] = useState([]);
+	//   const [formObject, setFormObject] = useState({})
+	const [parks, setParks] = useState([]);
+	//   // Load all parks and store them with setParks
+	useEffect(() => {
+		getGoogleMap('38.57933,-121.4909', 1500, 'park').then(({ data }) => {
+			console.log(data);
+			setParks(data);
+			// setName()
 
-      //     //   Load up the name
-      //       setName(parks)
-      //     //   Load up a photo
-      //     setPhotos()
-      //     // Load up the address
-      //     setVicinity()
-      //     // State if the park is open
-      //     setBiz()
+			//     //   Load up the name
+			//       setName(parks)
+			//     //   Load up a photo
+			//     setPhotos()
+			//     // Load up the address
+			//     setVicinity()
+			//     // State if the park is open
+			//     setBiz()
 
-      //     //   loadParks(parks)
-      //       console.log(parks);
-    });
-  }, []);
-  // Loads all books and sets them to books
-  //   function loadParks() {
-  //     getGoogleMap("38.57933,-121.4909", 1500, "park")
-  //       // .then(res =>
-  //       // =====
-  //       // FOR SETTING THE DATA INTO A FUNCTION AND LOAD INTO A DATABASE
-  //       // setGoogleMap(res.data)
-  //       // ====
-  //       // )
-  //       .catch((err) => console.log(err));
-  //   }
+			//     //   loadParks(parks)
+			//       console.log(parks);
+		});
+	}, []);
+	// Loads all books and sets them to books
+	//   function loadParks() {
+	//     getGoogleMap("38.57933,-121.4909", 1500, "park")
+	//       // .then(res =>
+	//       // =====
+	//       // FOR SETTING THE DATA INTO A FUNCTION AND LOAD INTO A DATABASE
+	//       // setGoogleMap(res.data)
+	//       // ====
+	//       // )
+	//       .catch((err) => console.log(err));
+	//   }
 
-  // render(){
-  return (
-    <Container style={{ minHeight: "80%" }}>
-      <h1 className="text-center">Parks In Your Area</h1>
-      <h2 className="text-center">Added just to commit</h2>
+	// render(){
+	return (
+		<Container style={{ minHeight: '80%' }}>
+			<h1 className="text-center">Parks In Your Area</h1>
+			<h2 className="text-center">Added just to commit</h2>
 
-      <div className="row">
-        {parks.map((park) => {
-          const imgSrc = park.photos
-            ? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${park.photos[0].photo_reference}&key=${googleApiKey}`
-            : "https://imgix.ranker.com/user_node_img/94/1872415/original/randall-park-people-in-tv-photo-u1?w=650&q=50&fm=pjpg&fit=crop&crop=faces"; //replace '' with placeholder img url
+			<div className="row parksDiv">
+				{parks.map((park) => {
+					const imgSrc = park.photos
+						? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${park.photos[0].photo_reference}&key=${googleApiKey}`
+						: 'https://imgix.ranker.com/user_node_img/94/1872415/original/randall-park-people-in-tv-photo-u1?w=650&q=50&fm=pjpg&fit=crop&crop=faces'; //replace '' with placeholder img url
 
-          return (
-            <div className="card col-3">
-              <img alt="biteme" src={imgSrc} />
-              <div>Park Name: {park.name}</div>
-              <div>Park Address: {park.vicinity}</div>
-              <div>Is the Park Open?: {park.business_status}</div>
-              <Link to={`/park/${park.place_id}`}>
-                <button
-                  onClick={() => setPark({ ...park, imgSrc })}
-                  className="btn btn-lg btn-success"
-                >
-                  Details
-                </button>
-              </Link>
-            </div>
-          );
-        })}
-      </div>
-    </Container>
-  );
+					return (
+						<div className="card col-3">
+							<img alt="biteme" src={imgSrc} />
+							<div>Park Name: {park.name}</div>
+							<div>Park Address: {park.vicinity}</div>
+							<div>Is the Park Open?: {park.business_status}</div>
+							<Link to={`/park/${park.place_id}`}>
+								<button
+									onClick={() => setPark({ ...park, imgSrc })}
+									className="btn btn-lg btn-success"
+								>
+									Details
+								</button>
+							</Link>
+						</div>
+					);
+				})}
+			</div>
+		</Container>
+	);
 
-  //  ===================================================================================================
-  // The foloowing code was removed by Danny. We should review it prior to deleting from the code
+	//  ===================================================================================================
+	// The foloowing code was removed by Danny. We should review it prior to deleting from the code
 
-  function Maps() {
-    //   const [developerState, setDeveloperState] = useState({
-    //     name: [],
-    //     photos: [],
-    //     vicinity: [],
-    //     business_status:""
-    //   });
-    //data destructuring
-    // ================= Hooks for setting data ==============
-    // Setting our component's initial state
-    //   const [name, setName] = useState([]);
-    //   const [photos, setPhotos] = useState([]);
-    //   const [vicinity, setVicinity] = useState([]);
-    //   const [business_status, setBiz] = useState([]);
-    //   const [formObject, setFormObject] = useState({})
-    // 	const [parks, setParks] = useState([]);
-    //   // Load all parks and store them with setParks
-    // 	useEffect(() => {
-    // 		getGoogleMap('38.57933,-121.4909', 1500, 'park').then(({ data }) => {
-    // 			console.log(data.results);
-    // 			setParks(data.results);
-    // setName()
-    //     //   Load up the name
-    //       setName(parks)
-    //     //   Load up a photo
-    //     setPhotos()
-    //     // Load up the address
-    //     setVicinity()
-    //     // State if the park is open
-    //     setBiz()
-    //     //   loadParks(parks)
-    //       console.log(parks);
-    // 		});
-    // 	}, []);
-    // Loads all books and sets them to books
-    //   function loadParks() {
-    //     getGoogleMap("38.57933,-121.4909", 1500, "park")
-    //       // .then(res =>
-    //       // =====
-    //       // FOR SETTING THE DATA INTO A FUNCTION AND LOAD INTO A DATABASE
-    //       // setGoogleMap(res.data)
-    //       // ====
-    //       // )
-    //       .catch((err) => console.log(err));
-    //   }
-    // render(){
-    // 	return (
-    // 		<Container style={{ minHeight: '80%' }}>
-    // 			<h1 className="text-center">Parks In Your Area</h1>
-    // 			<h2 className="text-center">Added just to commit</h2>
-    // 			<div className="row">
-    // 				{parks.map((park) => {
-    // 					const imgSrc = park.photos
-    // 						? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${park.photos[0].photo_reference}&key=${googleApiKey}`
-    // 						: 'https://imgix.ranker.com/user_node_img/94/1872415/original/randall-park-people-in-tv-photo-u1?w=650&q=50&fm=pjpg&fit=crop&crop=faces'; //replace '' with placeholder img url
-    // 					return (
-    // 						<div className="card col-3">
-    // 							<img alt="biteme" src={imgSrc} />
-    // 							<div>Park Name: {park.name}</div>
-    // 							<div>Park Address: {park.vicinity}</div>
-    // 							<div>Is the Park Open?: {park.business_status}</div>
-    // 							<div href="#"></div>
-    // 						</div>
-    // 					);
-    // 				})}
-    // 			</div>
-    // 		</Container>
-    // 	);
-    // ==========================================================================================
-  }
+	function Maps() {
+		//   const [developerState, setDeveloperState] = useState({
+		//     name: [],
+		//     photos: [],
+		//     vicinity: [],
+		//     business_status:""
+		//   });
+		//data destructuring
+		// ================= Hooks for setting data ==============
+		// Setting our component's initial state
+		//   const [name, setName] = useState([]);
+		//   const [photos, setPhotos] = useState([]);
+		//   const [vicinity, setVicinity] = useState([]);
+		//   const [business_status, setBiz] = useState([]);
+		//   const [formObject, setFormObject] = useState({})
+		// 	const [parks, setParks] = useState([]);
+		//   // Load all parks and store them with setParks
+		// 	useEffect(() => {
+		// 		getGoogleMap('38.57933,-121.4909', 1500, 'park').then(({ data }) => {
+		// 			console.log(data.results);
+		// 			setParks(data.results);
+		// setName()
+		//     //   Load up the name
+		//       setName(parks)
+		//     //   Load up a photo
+		//     setPhotos()
+		//     // Load up the address
+		//     setVicinity()
+		//     // State if the park is open
+		//     setBiz()
+		//     //   loadParks(parks)
+		//       console.log(parks);
+		// 		});
+		// 	}, []);
+		// Loads all books and sets them to books
+		//   function loadParks() {
+		//     getGoogleMap("38.57933,-121.4909", 1500, "park")
+		//       // .then(res =>
+		//       // =====
+		//       // FOR SETTING THE DATA INTO A FUNCTION AND LOAD INTO A DATABASE
+		//       // setGoogleMap(res.data)
+		//       // ====
+		//       // )
+		//       .catch((err) => console.log(err));
+		//   }
+		// render(){
+		// 	return (
+		// 		<Container style={{ minHeight: '80%' }}>
+		// 			<h1 className="text-center">Parks In Your Area</h1>
+		// 			<h2 className="text-center">Added just to commit</h2>
+		// 			<div className="row">
+		// 				{parks.map((park) => {
+		// 					const imgSrc = park.photos
+		// 						? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${park.photos[0].photo_reference}&key=${googleApiKey}`
+		// 						: 'https://imgix.ranker.com/user_node_img/94/1872415/original/randall-park-people-in-tv-photo-u1?w=650&q=50&fm=pjpg&fit=crop&crop=faces'; //replace '' with placeholder img url
+		// 					return (
+		// 						<div className="card col-3">
+		// 							<img alt="biteme" src={imgSrc} />
+		// 							<div>Park Name: {park.name}</div>
+		// 							<div>Park Address: {park.vicinity}</div>
+		// 							<div>Is the Park Open?: {park.business_status}</div>
+		// 							<div href="#"></div>
+		// 						</div>
+		// 					);
+		// 				})}
+		// 			</div>
+		// 		</Container>
+		// 	);
+		// ==========================================================================================
+	}
 }
 
 export default Maps;
--- utils/auth.js
+++ utils/auth.js
@@ -1,7 +1,9 @@
 const withAuth = (req, res, next) => {
 	if (!req.session.logged_in) {
+		console.log('Unauthorized');
 		res.redirect('/login');
 	} else {
+		console.log('Authorized');
 		next();
 	}
 };
