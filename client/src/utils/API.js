a480abe853eca686022b00d600554f7d0ce9233b
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-02-25 22:18:39 -0800

    Fixed the fucking goddamn API call

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-// API Key that is loaded in .env file
-const googleApiKey = process.env.REACT_APP_googleApiKey;
 // User routes
 const signup = (data) => axios.post('/api/user/signup', data);
 const login = (data) => axios.post('/api/user/login', data);
@@ -18,28 +16,11 @@ const updateMembers = (teamData) => axios.put('api/user/team/:id', teamData);
 const getUser = () => axios.get('/api/user/current_user');
 const logout = () => axios.get('/api/user/logout');
 
-export const getGoogleMap = (location, radius, type) => {
-	const mapsURL =
-		`/api/google/parksearch/${encodeURIComponent(location)}/${radius}/${type}`
-		// //location lat & lon
-		// // "33.771713,-118.18131"
-		// location +
-		// '&radius=' +
-		// //radius in a circular meter
-		// // "1500"
-		// radius +
-		// '&type=' +
-		// //place type ie Park, Restaurant, Movie-Theater, etc
-		// // "park"
-		// type +
-		// '&key=' +
-		// googleApiKey;
-
-	//  console.log("API.js " + mapsURL);
-	return axios.get(mapsURL);
+export const getGoogleMap = (data) => {
+	return axios.post("/api/google/search", data);
 };
 
-export default {
+const API = {
 	getGoogleMap,
 	signup,
 	login,
@@ -50,3 +31,5 @@ export default {
 	getUser,
 	logout,
 };
+
+export default API;
--- routes/api/index.js
+++ routes/api/index.js
@@ -5,5 +5,9 @@ const googleroutes = require("./googleAPI");
 // Book routes
 router.use("/user", userRoutes);
 router.use("/google", googleroutes);
+router.get("/test", (req,res) =>{
+    console.log("CONSOLE LOG TEST");
+    res.json("HELLO TEST");
+});
 
 module.exports = router;
