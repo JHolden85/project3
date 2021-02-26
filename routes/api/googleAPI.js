d6f23b4f315f23f345a4b33f22fab59f3d050cf2
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-02-25 20:54:58 -0800

    Troubleshooting server

--- routes/api/googleAPI.js
+++ routes/api/googleAPI.js
@@ -22,4 +22,28 @@ router.post("/search", (req, res) => {
         axios.get(mapsURL).then(({data}) => res.json(data.results));
 })
 
+router.get("/parksearch/:location/:radius/:type", (req, res) => {
+        const {location, radius, type} = req.params;
+        
+        console.log("HEY I MADE IT HERE! ",location, radius, type)
+        const mapsURL =
+          "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" +
+          //location lat & lon
+          // "33.771713,-118.18131" 
+          location +
+          "&radius=" +
+          //radius in a circular meter
+          // "1500" 
+          radius +
+          "&type=" +
+          //place type ie Park, Restaurant, Movie-Theater, etc
+          // "park" 
+          type +
+          "&key=" +
+         process.env.googleApiKey;
+         
+        //  console.log("API.js " + mapsURL);
+        axios.get(mapsURL).then(({data}) => res.json(data.results));
+})
+
 module.exports = router
