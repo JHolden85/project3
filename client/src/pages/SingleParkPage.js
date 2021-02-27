8b21f6d13903744f957cb4c15cb8968316b80ddf
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-02-27 13:08:21 -0800

    Added props into the singleParkPage

--- client/src/pages/SingleParkPage.js
+++ client/src/pages/SingleParkPage.js
@@ -1,5 +1,4 @@
 
-
 import Container from "../components/Container"
 const googleApiKey = process.env.REACT_APP_googleApiKey;
 
@@ -11,12 +10,11 @@ export default function ParkPage(props) {
     <Container style={{ minHeight: "80%" }}>
         <h1 className="text-center">{props.name}</h1>
         <div className="row">
-        {/* {props.map((park) => {
-          const imgSrc = park.photos
-            ? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${park.photos[0].photo_reference}&key=${googleApiKey}`
-            : "https://imgix.ranker.com/user_node_img/94/1872415/original/randall-park-people-in-tv-photo-u1?w=650&q=50&fm=pjpg&fit=crop&crop=faces"; */}
+        <img alt="Bite-Me" src=""/>
     <h2 className="text-center">{props.vicinity}:{props.business_status}</h2>
-    
+    <div className="row">
+
+    </div>
      {/* <img alt="biteme" src={props.imgSrc}/> */}
     </div>
         
@@ -24,5 +22,4 @@ export default function ParkPage(props) {
         
     );
 }
-
 
