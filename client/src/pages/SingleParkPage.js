6621cb454ff2472c30ddb70c28ec1922d8ff9047
Daniel Figueroa <dfigueroa7007@gmail.com>
2021-03-02 22:42:20 -0700

    updated page to show park address and rating.

--- client/src/pages/SingleParkPage.js
+++ client/src/pages/SingleParkPage.js
@@ -1,19 +1,19 @@
-
-
-import Container from "../components/Container"
+import Container from "../components/Container";
 
 export default function ParkPage(props) {
-
-
-    return (
+  return (
     <Container style={{ minHeight: "80%" }}>
-        <h1 className="text-center">{props.name}</h1>
-        <div className="row">
-    <h1>{props.data}</h1>
-    <img alt="biteme" src={props.imgSrc}/>
-    </div>
+      <h1 className="text-center">{props.name}</h1>
+      <div className="row">
+        <h1>{props.data}</h1>
+        <img alt="biteme" src={props.imgSrc} />
+      </div>
+      <div>
+          <h4>{props.vicinity}</h4>
+      </div>
+      <div>
+          <h4>Community Rating: {props.rating}</h4>
+      </div>
     </Container>
-    );
+  );
 }
-
-
