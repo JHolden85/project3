3209d6fd79797f23be6998e34dcc8f3881e1ddbf
Carlos <perez.carlos_a@yahoo.com>
2021-03-03 22:12:46 -0800

    Single parks page now generates image

--- client/src/pages/SingleParkPage.js
+++ client/src/pages/SingleParkPage.js
@@ -1,26 +1,30 @@
-import Container from "../components/Container";
+import Container from '../components/Container';
 
 export default function ParkPage(props) {
-  return (
-    <Container style={{ minHeight: "80%" }}>
-      <h1 className="text-center">{props.name}</h1>
-      <div className="row">
-        <h1>{props.data}</h1>
-        <img alt="biteme" src={props.imgSrc} />
-      </div>
-      <div>
-          <h4>{props.vicinity}</h4>
-      </div>
-      <div>
-          <h4>Community Rating: {props.rating}</h4>
-      </div>
-    </Container>
-  );
+	const googleApiKey = process.env.REACT_APP_googleApiKey;
+
+	const imgSrc = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${props.photos[0].photo_reference}&key=${googleApiKey}`;
+
+	console.log('passed in props to the single page', props);
+	return (
+		<Container style={{ minHeight: '80%' }}>
+			<h1 className="text-center">{props.name}</h1>
+			<div className="row">
+				<h1>{props.data}</h1>
+				<img alt="biteme" src={imgSrc} />
+			</div>
+			<div>
+				<h4>{props.vicinity}</h4>
+			</div>
+			<div>
+				<h4>Community Rating: {props.rating}</h4>
+			</div>
+		</Container>
+	);
 }
 
-
 // ======================
-    
+
 //     const UploadPic = function(img) {
 //         ////const {place_id} = useParams()
 //         console.log(img.target.value)
