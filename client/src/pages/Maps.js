b123b8ab21ddece113e7ab71c83c62dcdf199833
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-03-03 19:52:24 -0800

    Additional CSS to the Map.js

--- client/src/pages/Maps.js
+++ client/src/pages/Maps.js
@@ -1,5 +1,14 @@
 import React, { Component, useState, useEffect } from "react";
-import { MDBCard, MDBCardBody, MDBCardHeader, MDBCardTitle, MDBCardText, MDBCardImage, MDBBtn } from 'mdb-react-ui-kit';
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardGroup,
+  MDBCardHeader,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardImage,
+  MDBBtn,
+} from "mdb-react-ui-kit";
 import API from "../utils/API";
 // import the "SingleParkPage" to send the data from the maps.js
 
@@ -7,39 +16,39 @@ import API from "../utils/API";
 // import MapResults from "../components/mapResults";
 import { useHistory } from "react-router-dom";
 
-import Container from '../components/Container';
+import Container from "../components/Container";
 // import router from '../../../routes/api/userRoutes';
 
 const googleApiKey = process.env.REACT_APP_googleApiKey;
 // import MapResults from "../components/mapResults";
 
 // Setting the developer state
-function Maps({setPark}) {
+function Maps({ setPark }) {
   // ================= Hooks for setting data ==============
   const [parks, setParks] = useState([]);
   const history = useHistory();
   //Call to retreive the API data, and then load into the setParks function
   useEffect(() => {
-  API.getGoogleMap({
-    location: "33.771713,-118.18131",
-    radius: 10000,
-    type: "park",
-  })
-    .then(({ data }) => {
-      console.log("Success:", data);
-      setParks(data)
+    API.getGoogleMap({
+      location: "33.771713,-118.18131",
+      radius: 10000,
+      type: "park",
     })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
+      .then(({ data }) => {
+        console.log("Success:", data);
+        setParks(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
 
-  //   });
+    //   });
   }, []);
 
-  const handleSinglePark = data => {
+  const handleSinglePark = (data) => {
     setPark(data);
-    history.push("/park")
-  }
+    history.push("/park");
+  };
 
   return (
     <Container style={{ minHeight: "80%" }}>
@@ -52,20 +61,28 @@ function Maps({setPark}) {
             : "https://imgix.ranker.com/user_node_img/94/1872415/original/randall-park-people-in-tv-photo-u1?w=650&q=50&fm=pjpg&fit=crop&crop=faces"; //replace '' with placeholder img url
 
           return (
-            // <div className="card col-4 ourParkCards square border border-success" onClick={() => handleSinglePark(park)} key={park.place_id}>
-              <MDBCard alignment='center' style={{ maxWidth: '20em' }}>
-              {/* <img className="cardImg" alt="Imagine A Happy Little Tree" src={imgSrc} /> */}
-              <MDBCardHeader>{park.name}</MDBCardHeader>
-              <MDBCardImage src={imgSrc} position='top' alt="Park image here"/>
-              {/* <div className="text-center">Park Name: {park.name}</div> */}
-              <MDBCardText>Park Address: {park.vicinity}</MDBCardText>              
-              {/* <div className="text-center">Park Address: {park.vicinity}</div> */}
-              <MDBCardTitle>The Park is: {park.business_status}</MDBCardTitle>
-              {/* <div className="text-center">Is the Park Open?: {park.business_status}</div> */}
-              <MDBBtn onClick={() => handleSinglePark(park)} key={park.place_id}>Click Here To Discover More!</MDBBtn>
-              {/* <div>Park Id:</div> */}
-              {/* <button onClick={API.getGoogleMap} type="button"></button> */}
-            {/* </div> */}
+            <MDBCard alignment="center" style={{ maxWidth: "20em" }}>
+              
+              <MDBCardBody>
+                <MDBCardTitle>{park.name}</MDBCardTitle>
+                <MDBCardImage
+                  src={imgSrc}
+                  position="top"
+                  alt="Park image here"
+                />
+                
+                <MDBCardText>Park Address: {park.vicinity}</MDBCardText>
+                
+                <MDBCardTitle>The Park is: {park.business_status}</MDBCardTitle>
+                
+                <MDBBtn
+                  onClick={() => handleSinglePark(park)}
+                  key={park.place_id}
+                >
+                  Click Here To Discover More!
+                </MDBBtn>
+               
+              </MDBCardBody>
             </MDBCard>
           );
         })}
