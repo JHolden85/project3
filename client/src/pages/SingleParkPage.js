37c06ea6c3eb0033dfdc41e899eee3fc603ccb4a
Carlos <perez.carlos_a@yahoo.com>
2021-03-03 23:05:19 -0800

    added styling to single park page and linked address to google maps

--- client/src/pages/SingleParkPage.js
+++ client/src/pages/SingleParkPage.js
@@ -1,23 +1,58 @@
 import Container from '../components/Container';
 
+const style = {
+	parentDiv: {
+		marginBottom: '60px',
+		backgroundColor: 'green',
+		margin: '10px 10px 100px 10px',
+		padding: '10px',
+		color: 'white',
+		border: '2px solid darkgreen',
+		borderRadius: '15px',
+		textShadow: '1px 1px black',
+	},
+	image: {
+		display: 'block',
+		marginLeft: 'auto',
+		marginRight: 'auto',
+		width: '80%',
+		border: '1.5px solid darkgreen',
+		borderRadius: '15px',
+	},
+	a: {
+		color: 'white',
+	},
+};
+
 export default function ParkPage(props) {
 	const googleApiKey = process.env.REACT_APP_googleApiKey;
 
-	const imgSrc = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${props.photos[0].photo_reference}&key=${googleApiKey}`;
+	const imgSrc = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=350&photoreference=${props.photos[0].photo_reference}&key=${googleApiKey}`;
 
-	console.log('passed in props to the single page', props);
 	return (
 		<Container style={{ minHeight: '80%' }}>
-			<h1 className="text-center">{props.name}</h1>
-			<div className="row">
-				<h1>{props.data}</h1>
-				<img alt="biteme" src={imgSrc} />
-			</div>
-			<div>
-				<h4>{props.vicinity}</h4>
-			</div>
-			<div>
-				<h4>Community Rating: {props.rating}</h4>
+			<div style={style.parentDiv}>
+				<h1 className="text-center">{props.name}</h1>
+				<div className="row">
+					<h1>{props.data}</h1>
+					<img alt="biteme" style={style.image} src={imgSrc} />
+				</div>
+				<div>
+					<br></br>
+					<h4>
+						<a
+							href={`https://maps.google.com/?ll=${props.geometry.location.lat},${props.geometry.location.lng}`}
+							target="_blank"
+							rel="noreferrer"
+							style={style.a}
+						>
+							{props.vicinity}
+						</a>
+					</h4>
+				</div>
+				<div>
+					<h4>Community Rating: {props.rating}</h4>
+				</div>
 			</div>
 		</Container>
 	);
