a1601447dfab002069aa66cd172a208775ee127e
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-03-05 20:51:27 -0800

    Brough over the CSS changes created by Carlos

--- client/src/pages/Maps.js
+++ client/src/pages/Maps.js
@@ -1,104 +1,141 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { Component, useState, useEffect } from 'react';
 import {
-  MDBCard,
-  MDBCardBody,
-  MDBCardGroup,
-  MDBCardHeader,
-  MDBCardTitle,
-  MDBCardText,
-  MDBCardImage,
-  MDBBtn,
-  MDBRow,
-  MDBCol,
-  MDBCardFooter
-} from "mdb-react-ui-kit";
-import API from "../utils/API";
+	MDBCard,
+	MDBCardBody,
+	MDBCardGroup,
+	MDBCardHeader,
+	MDBCardTitle,
+	MDBCardText,
+	MDBCardImage,
+	MDBBtn,
+	MDBRow,
+	MDBCol,
+	MDBCardFooter,
+} from 'mdb-react-ui-kit';
+import API from '../utils/API';
+
 // import the "SingleParkPage" to send the data from the maps.js
-// Modifying for Heroku commit
 
 // import the mapResultsPage as a component
 // import MapResults from "../components/mapResults";
 import { useHistory } from 'react-router-dom';
 
-import Container from "../components/Container";
+import Container from '../components/Container';
 // import router from '../../../routes/api/userRoutes';
 
 const googleApiKey = process.env.REACT_APP_googleApiKey;
 // import MapResults from "../components/mapResults";
 
+// const style = {
+// 	card: {
+// 		display: 'block',
+// 		right: '10px',
+// 		margin: 'auto',
+// 		width: '90%',
+// 		color: 'white',
+// 	},
+// 	cardBody: {
+// 		border: '2px solid darkgreen',
+// 		borderRadius: '15px',
+// 		backgroundColor: 'green',
+// 		// marginTop: 'px',
+// 		maxwidth: '20em',
+// 	},
+// 	color: {
+// 		color: 'white',
+// 	},
+// 	header: {
+// 		marginTop: '10px',
+// 		marginBottom: '30px',
+// 		fontSize: '250%',
+// 	},
+// };
+
 // Setting the developer state
 function Maps({ setPark }) {
-  // ================= Hooks for setting data ==============
-  const [parks, setParks] = useState([]);
-  const history = useHistory();
-  //Call to retreive the API data, and then load into the setParks function
-  useEffect(() => {
-    API.getGoogleMap({
-      location: "33.771713,-118.18131",
-      radius: 10000,
-      type: "park",
-    })
-      .then(({ data }) => {
-        console.log("Success:", data);
-        setParks(data);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
+	// ================= Hooks for setting data ==============
+	const [parks, setParks] = useState([]);
+	const history = useHistory();
+	//Call to retreive the API data, and then load into the setParks function
+	useEffect(() => {
+		API.getGoogleMap({
+			location: '33.771713,-118.18131',
+			radius: 10000,
+			type: 'park',
+		})
+			.then(({ data }) => {
+				console.log('Success:', data);
+				setParks(data);
+			})
+			.catch((error) => {
+				console.error('Error:', error);
+			});
 
-    //   });
-  }, []);
+		//   });
+	}, []);
 
-  const handleSinglePark = (data) => {
-    setPark(data);
-    history.push("/park");
-  };
+	const handleSinglePark = (data) => {
+		setPark(data);
+		history.push('/park');
+	};
 
-  return (
-    <Container style={{ minHeight: "80%" }} id="mapsCardContainer">
-      <h1 className="text-center">Parks in your Area</h1>
-      <MDBRow className="row-cols-1 row-cols-md-2 g-4">
-        
-          {parks.map((park) => {
-            const imgSrc = park.photos
-              ? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${park.photos[0].photo_reference}&key=${googleApiKey}`
-              : "https://imgix.ranker.com/user_node_img/94/1872415/original/randall-park-people-in-tv-photo-u1?w=650&q=50&fm=pjpg&fit=crop&crop=faces"; //replace '' with placeholder img url
+	return (
+		<Container style={{ minHeight: '80%' }} id="mapsCardContainer">
+			<h1 className="text-center" >
+      {/* style={style.header} */}
+				Parks in your Area
+			</h1>
+			<MDBRow className="row-cols-1 row-cols-md-2 g-4">
+				{parks.map((park) => {
+					const imgSrc = park.photos
+						? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${park.photos[0].photo_reference}&key=${googleApiKey}`
+						: 'https://imgix.ranker.com/user_node_img/94/1872415/original/randall-park-people-in-tv-photo-u1?w=650&q=50&fm=pjpg&fit=crop&crop=faces'; //replace '' with placeholder img url
 
-            return (
-              <MDBCol>
-              <MDBCard alignment="center" style={{ maxWidth: "20em" }}>
-                <MDBCardBody>
-                  <MDBCardTitle>{park.name}</MDBCardTitle>
-                  <MDBCardImage
-                    src={imgSrc}
-                    position="top"
-                    alt="Park image here"
-                  />
+					return (
+						<MDBCol key={park.place_id}>
+               {/* style={style.card} */}
+							<MDBCard alignment="center" >
+              {/* style={style.cardBody} */}
+								<MDBCardBody>
+									<MDBCardTitle>
+										<big>{park.name}</big>
+									</MDBCardTitle>
+									<MDBCardImage
+										src={imgSrc}
+										position="top"
+										alt="Park image here"
+									/>
 
-                  <MDBCardText>Park Address: {park.vicinity}</MDBCardText>
+									<MDBCardText>
+										Park Address:
+										<br></br>
+										{park.vicinity}
+									</MDBCardText>
 
-                  {/* <MDBCardTitle>
+									{/* <MDBCardTitle>
                     The Park is: 
                   </MDBCardTitle> */}
 
-                  <MDBBtn
-                    onClick={() => handleSinglePark(park)}
-                    key={park.place_id}
-                  >
-                    Click Here To Discover More!
-                  </MDBBtn>
-                </MDBCardBody>
-                <MDBCardFooter>
-                  <small className='text-muted'>{park.business_status}</small>
-                </MDBCardFooter>
-              </MDBCard>
-              </MDBCol>
-            );
-          })}
-        
-      </MDBRow>
-    </Container>
-  );
+									<MDBBtn
+										onClick={() => handleSinglePark(park)}
+										key={park.place_id}
+									>
+										Click Here To Discover More!
+									</MDBBtn>
+								</MDBCardBody>
+								<MDBCardFooter>
+									<small className="text" >
+                  {/* style={style.color} */}
+										{park.business_status}
+									</small>
+								</MDBCardFooter>
+							</MDBCard>
+						</MDBCol>
+					);
+				})}
+			</MDBRow>
+		</Container>
+	);
 }
 
 export default Maps;
