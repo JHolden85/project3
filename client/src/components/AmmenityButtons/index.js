ed15629602068d9483345aee4ae8ac144cc0baa8
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-03-25 10:03:36 -0700

    Added a functioning button to the AmmBtn component

--- client/src/components/AmmenityButtons/index.js
+++ client/src/components/AmmenityButtons/index.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Button from '@material-ui/core/Button';
+
 // Create multiple buttons that toggle "on/off" for this component
 const AmmBtn = () => {
     return (
         <div className="button-container">
-            <Button variant= "contained">Basketball</Button>
+            <button onClick="">Basketball</button>
         </div>
     )
 }
