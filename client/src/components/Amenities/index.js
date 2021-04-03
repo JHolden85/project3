7fa19f6a178a3d5fd22761413d6a5f38a1cf33ce
Carlos <perez.carlos_a@yahoo.com>
2021-04-02 19:50:22 -0700

    css changes to single park page amenities section

--- client/src/components/Amenities/index.js
+++ client/src/components/Amenities/index.js
@@ -2,52 +2,52 @@ import React from 'react';
 import './style.css';
 
 const Amenities = ({ park }) => {
-	console.log('park data received on amenities page', park);
-
 	return (
-		<div className="Amenities row">
-			<ul className="col">
-				<li>Baseball:</li>
-				<hr></hr>
-				<li>Soccer:</li>
-				<hr></hr>
-				<li>Football:</li>
-				<hr></hr>
-				<li>Basketball:</li>
-				<hr></hr>
-				<li>Running Track:</li>
-				<hr></hr>
-				<li>Handball:</li>
-				<hr></hr>
-				<li>Tennis:</li>
-				<hr></hr>
-				<li>Trail:</li>
-				<hr></hr>
-				<li>Pool:</li>
-				<hr></hr>
-				<li>Running Track:</li>
-			</ul>
-			<ul className="col">
-				<li>true</li>
-				<hr></hr>
-				<li>true</li>
-				<hr></hr>
-				<li>true</li>
-				<hr></hr>
-				<li>true</li>
-				<hr></hr>
-				<li>true</li>
-				<hr></hr>
-				<li>true</li>
-				<hr></hr>
-				<li>true</li>
-				<hr></hr>
-				<li>true</li>
-				<hr></hr>
-				<li>true</li>
-				<hr></hr>
-				<li>true</li>
-			</ul>
+		<div className="Amenities">
+			<div className=" row">
+				<ul className="col">
+					<li>Baseball:</li>
+					<hr></hr>
+					<li>Soccer:</li>
+					<hr></hr>
+					<li>Football:</li>
+					<hr></hr>
+					<li>Basketball:</li>
+					<hr></hr>
+					<li>Running Track:</li>
+					<hr></hr>
+					<li>Handball:</li>
+					<hr></hr>
+					<li>Tennis:</li>
+					<hr></hr>
+					<li>Trail:</li>
+					<hr></hr>
+					<li>Pool:</li>
+					<hr></hr>
+					<li>Running Track:</li>
+				</ul>
+				<ul className="col">
+					<li>true</li>
+					<hr></hr>
+					<li>true</li>
+					<hr></hr>
+					<li>true</li>
+					<hr></hr>
+					<li>true</li>
+					<hr></hr>
+					<li>true</li>
+					<hr></hr>
+					<li>true</li>
+					<hr></hr>
+					<li>true</li>
+					<hr></hr>
+					<li>true</li>
+					<hr></hr>
+					<li>true</li>
+					<hr></hr>
+					<li>true</li>
+				</ul>
+			</div>
 		</div>
 	);
 };
